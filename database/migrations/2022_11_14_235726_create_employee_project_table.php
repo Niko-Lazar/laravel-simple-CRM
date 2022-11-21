ce93e17eeab614d6749ab93d1a203019d2c37ca1
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-21 07:10:31 +0100

    employee_project table foreign keys are constrained now

--- database/migrations/2022_11_14_235726_create_employee_project_table.php
+++ database/migrations/2022_11_14_235726_create_employee_project_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('employee_project', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employee_id');
-            $table->foreignId('project_id');
+            $table->foreignId('employee_id')->constrained();
+            $table->foreignId('project_id')->constrained();
             $table->timestamps();
         });
     }
