7e68dd30dbe9c75bdef0433c1393cfdf8003cfbc
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-27 11:57:31 +0100

    removed commented code

--- app/Actions/Auth/RegisterUser.php
+++ app/Actions/Auth/RegisterUser.php
@@ -9,11 +9,6 @@ class registerUser
 {
     public function handle(StoreUserRequest $request) : bool
     {
-//        if(!(request('key') === config('app.register_key'))){
-//
-//            return false;
-//        }
-
         $attributes = $request->validated();
 
         User::create($attributes);
