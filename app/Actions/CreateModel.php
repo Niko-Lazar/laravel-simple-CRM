9a19adba60377dadff7fe37b5da9c63194901e9e
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-29 17:32:35 +0100

    removed unnecessary constructor

--- app/Actions/CreateModel.php
+++ app/Actions/CreateModel.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateModel
 {
-    public function __construct(StoreFile $storeFile)
-    {
-        $storeFile = new StoreFile();
-    }
-
     public function handle(string $model, FormRequest $request) : Model
     {
         $attributes = $request->validated();
